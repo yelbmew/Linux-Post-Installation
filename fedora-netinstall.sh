31f0fb96290c244477ac194c773969e2eb9518aa
yelbmew <yelbmewo@gmail.com>
2019-01-08 23:22:27 +1100

    Day:008 RepoDay:001 - init push + Arch and Fedora post installation script

---
+++ fedora-netinstall.sh
@@ -0,0 +1,22 @@
+# test on Fedora 29
+# Repo
+snap
+
+# Tools
+i3
+vim
+tmux
+sxiv  # a better for feh image viewer
+htop
+s-tui
+tree
+lightdm
+lightdm-slick-greeter
+transmission
+transmission-daemon
+
+
+# Security
+ykclients*
+ykpers*
+pam_yubico
