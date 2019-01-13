42a6b5e2e14a50ac700af33dfbf521cb9af4d8e5
yelbmew <yelbmewo@gmail.com>
2019-01-13 23:23:32 +1100

    add vim plugin manager Vundle to Fedora

--- fedora-netinstall.sh
+++ fedora-netinstall.sh
@@ -46,11 +46,11 @@ dots=(
         https://raw.githubusercontent.com/yelbmew/Public-Dot-Files/master/.bashrc
         https://raw.githubusercontent.com/yelbmew/Public-Dot-Files/master/.tmux.conf
         https://raw.githubusercontent.com/yelbmew/Public-Dot-Files/master/.vimrc
-     ); cd $HOME/temp; for i in ${dots[@]}; do curl -O $i; done
+     ); cd $HOME; for i in ${dots[@]}; do curl -O $i; done
 
 
-if [ -e $HOME/temp/bashrc ]; then
-cat >> $HOME/temp/bashrc <<- EOF
+if [ -e $HOME/.bashrc ]; then
+cat >> $HOME/.bashrc <<- EOF
 transfer() { if [ $# -eq 0 ];
 then echo -e "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi
 tmpfile=\$( mktemp -t transferXXX ); if tty -s; then basefile=\$(basename "\$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g');
@@ -59,11 +59,14 @@ else curl --progress-bar --upload-file "-" "https://transfer.sh/\$1" >> \$tmpfil
 EOF
 fi 
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# implement git package manager
-    # vundle
-    # how to automate vundle install ?
-#
+if [ -d $HOME/.vim ]; then
+    mkdir -p $HOME/.vim/colors && cd $HOME/.vim/colors
+    curl -o lucario.vim https://raw.githubusercontent.com/raphamorim/lucario/master/colors/lucario.vim
+fi
+
+vim +PluginInstall +qall
 
 # systemctl enable lightdm.service
 # systemctl set-default graphical.service
