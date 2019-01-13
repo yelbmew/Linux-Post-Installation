3022275ba82e52246b1b399f00730c43746546f2
yelbmew <yelbmewo@gmail.com>
2019-01-13 20:49:06 +1100

    add powerline path for fedora

--- README.md
+++ README.md
@@ -2,7 +2,3 @@
 1. Arch
 2. Fedora
 3. Ubuntu (Coming soon)
-
-Script note:
-10-01-2019: Need more tweak on Fedora script, transmission command is install but not found, s-tui not found
-11-01-2019: add transmission-cli (same tools with Arch Linux) but method of using are different
--- fedora-netinstall.sh
+++ fedora-netinstall.sh
@@ -2,46 +2,45 @@
 
 set -euxo pipefail \
 
-# Universal Package Manager
-sudo dnf install \
-    snapd 
+# ssh-keygen -t rsa -b 4096 -C "your@email" -f ~/.ssh/$USER
 
-# X Server
-dnf groupinstall -y \
-    base-x
+# # Universal Package Manager
+# sudo dnf install \
+#     snapd 
+# 
+# # X Server
+# dnf groupinstall -y \
+#     base-x
+# 
+# # Tools
+# dnf install -y \
+#     firefox \
+#     git \
+#     gnome-terminal \
+#     htop \
+#     i3 \
+#     lightdm \
+#     mosh \
+#     sxiv \
+#     tmux \
+#     transmission-cli \
+#     tree \
+#     unzip \
+#     vim 
+# 
+# 
+# # Yubikey
+#  dnf install -y \
+#      ykclient* \
+#      ykpers* \
+#      pam_yubico 
+# 
+# # powerline for bash and tmux
+# dnf install -y \
+#     powerline \
+#     powerline-fonts \
+#     tmux-powerline
 
-# Tools
-dnf install -y \
-    firefox \
-    git \
-    gnome-terminal \
-    htop \
-    i3 \
-    lightdm \
-    mosh \
-    sxiv \
-    tmux \
-    transmission-cli \
-    tree \
-    unzip \
-    vim 
-
-
-# Yubikey
- dnf install -y \
-     ykclient* \
-     ykpers* \
-     pam_yubico 
-
-# powerline for bash and tmux
-dnf install -y \
-    powerline \
-    powerline-fonts \
-    tmux-powerline
-
-bash="$HOME/.bashrc"
-tmux="$HOME/.tmux.conf"
-vim="$HOME/.vimrc"
 
 dots=(
         https://raw.githubusercontent.com/yelbmew/Public-Dot-Files/master/.bashrc
@@ -50,9 +49,9 @@ dots=(
      ); cd $HOME/temp; for i in ${dots[@]}; do curl -O $i; done
 
 
-if [ -e $HOME/bashrc ]; then
-cat >> $HOME/bashrc <<- EOF
-transfer() { if [ $# -eq 0 ];  # source: transfer.sh
+if [ -e $HOME/temp/bashrc ]; then
+cat >> $HOME/temp/bashrc <<- EOF
+transfer() { if [ $# -eq 0 ];
 then echo -e "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi
 tmpfile=\$( mktemp -t transferXXX ); if tty -s; then basefile=\$(basename "\$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g');
 curl --progress-bar --upload-file "\$1" "https://transfer.sh/\$basefile" >> \$tmpfile;
