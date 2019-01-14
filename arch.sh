f9da2c793fa62d05dfa598f078f0e0431e3c2ebe
yelbmew <yelbmewo@gmail.com>
2019-01-14 18:48:58 +1100

    update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,6 +1,9 @@
 #/usr/bin/bash
 
 set -euxo pipefail
+
+ssh-keygen -t rsa -b 4096 -C "your@email" -f ~/.ssh/$USER
+
 # X server
 yay -S --noconfirm \
     xrandr \
@@ -36,6 +39,42 @@ yay -s --noconfirm \
     s-tui \
     htop \
     mosh 
+
+
+# pull dot files from repo
+dots=(
+        https://raw.githubusercontent.com/yelbmew/Public-Dot-Files/master/.bashrc
+        https://raw.githubusercontent.com/yelbmew/Public-Dot-Files/master/.tmux.conf
+        https://raw.githubusercontent.com/yelbmew/Public-Dot-Files/master/.vimrc
+     ); cd $HOME; for i in ${dots[@]}; do curl -O $i; done
+
+
+# source: transfer.sh for easy/secure file sharing
+if [ -e $HOME/.bashrc ]; then
+cat >> $HOME/.bashrc <<- EOF
+transfer() { if [ \$# -eq 0 ];
+then echo -e "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi
+tmpfile=\$( mktemp -t transferXXX ); if tty -s; then basefile=\$(basename "\$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g');
+curl --progress-bar --upload-file "\$1" "https://transfer.sh/\$basefile" >> \$tmpfile;
+else curl --progress-bar --upload-file "-" "https://transfer.sh/\$1" >> \$tmpfile ; fi; cat \$tmpfile; rm -f \$tmpfile; }
+EOF
+fi 
+
+
+# Vundle plugin manger for Vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
+# pull Vim colorscheme
+if [ -d $HOME/.vim ]; then
+    mkdir -p $HOME/.vim/colors && cd $HOME/.vim/colors
+    curl -o lucario.vim https://raw.githubusercontent.com/raphamorim/lucario/master/colors/lucario.vim
+fi
+
+
+# install Vim plugin listed in .vimrc
+vim +PluginInstall +qall
+
 
 systemctl enable lightdm.service
 systemctl enable bluetooth.service
