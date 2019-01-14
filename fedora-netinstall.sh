6246872e98baf8067537637dad185126d7434e99
yelbmew <yelbmewo@gmail.com>
2019-01-14 14:53:25 +1100

    fix transfer.sh function

--- fedora-netinstall.sh
+++ fedora-netinstall.sh
@@ -51,7 +51,7 @@ dots=(
 
 if [ -e $HOME/.bashrc ]; then
 cat >> $HOME/.bashrc <<- EOF
-transfer() { if [ $# -eq 0 ];
+transfer() { if [ \$# -eq 0 ];
 then echo -e "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi
 tmpfile=\$( mktemp -t transferXXX ); if tty -s; then basefile=\$(basename "\$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g');
 curl --progress-bar --upload-file "\$1" "https://transfer.sh/\$basefile" >> \$tmpfile;
